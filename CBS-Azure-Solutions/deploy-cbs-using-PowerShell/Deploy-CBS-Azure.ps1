4a72546dcdd177b35842d16bec724a954ca071e7
Adam Mazouz <adam.mazouz.93@gmail.com>
2022-11-23 17:20:51 +0100

    Added PowerShell script for deploying CBS on Azure

---
+++ CBS-Azure-Solutions/deploy-cbs-using-PowerShell/Deploy-CBS-Azure.ps1
@@ -0,0 +1,129 @@
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "Enter the SubscriptionId where your Resource Group is located")]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript(
+        { $null -ne (Get-AzSubscription -SubscriptionId $_ -WarningAction silentlyContinue) },
+        ErrorMessage = "Subscription  was not found in tenant {0} . Please verify that the subscription exists in this tenant."
+    )]         
+    [string]
+    $subscriptionId,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter the region where your Resource group is located")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $region,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter zone")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $zone,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $arrayName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $orgDomain,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [securestring]
+    $licenseKey,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $applicationResourceName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $cbsVersion, 
+
+
+    [Parameter(Mandatory = $true, HelpMessage = "cbsModel v10 or v20")]
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern('(V10MUR1|V20MUR1)')] 
+    [string]
+    $cbsModel,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter your Virtual Network name")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $cbsVNETName,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter your System Subnet name")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $cbsSystemSubentName,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter your Management Subnet name")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $cbsMngmtSubentName,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter your iSCSI Subnet name")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $cbsiSCSiSubentName,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter your Replication Subnet name")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $cbsReplicaSubentName
+)
+
+# Hardcoded template file url
+$templateUri= "https://"
+
+
+## Connect to Azure Account
+# Connect-AzAccount
+
+# Select validated subscription 
+Set-AzContext -Subscription $subscriptionId -WarningAction silentlyContinue | Out-Null
+# Resource_Group
+$resourceGroup = (Get-AzVirtualNetwork -Name $cbsVNETName).ResourceGroupName
+if ($null -eq $resourceGroup) {
+    Write-Host "Caution: No virtual network found by the name '$cbsVNETName'."
+    Exit
+}
+
+$PSvnet = Get-AzVirtualNetwork -Name $cbsVNETName
+$PSSubnet = Get-AzVirtualNetworkSubnetConfig -Name $cbsSystemSubentName -VirtualNetwork $PSvnet
+if ($null -eq $PSSubnet) {
+    Write-Host "Caution: No subnet found by the name '$cbsSystemSubentName'"
+    Exit
+}
+
+New-AzResourceGroupDeployment `
+    -Name $applicationResourceName `
+    -ResourceGroupName $resourceGroup `
+    -TemplateUri $templateUri `
+    -applicationResourceName $applicationResourceName `
+    -arrayName $arrayName `
+    -sku $cbsModel `
+    -zone $zone `
+    -licenseKey $licenseKey `
+    -replicationResourceGroup $resourceGroup `
+    -systemResourceGroup $resourceGroup `
+    -managementResourceGroup $resourceGroup `
+    -iSCSIResourceGroup $resourceGroup `
+    -managementVnet $cbsVNETName `
+    -systemVnet $cbsVNETName `
+    -iSCSIVnet $cbsVNETName `
+    -replicationVnet $cbsVNETName `
+    -systemSubnet $cbsSystemSubentName `
+    -iSCSISubnet $cbsiSCSiSubentName `
+    -replicationSubnet $cbsReplicaSubentName `
+    -managementSubnet $cbsMngmtSubentName `
+    -alertRecipients $alertRecipients `
+    -orgDomain $orgDomain
+
+
+
+
+
---
+++ CBS-Azure-Solutions/deploy-cbs-using-PowerShell/README.md
@@ -0,0 +1,6 @@
+# Deploy Pure Cloud Block Store on Azure using AZ PowerShell Modules
+
+
+### Note: **To run this PowerShell script on your local Machine, make sure to install AZ modules and to sign into your Azure account. Alternatively, use can Azure Console Cloud Shell.**
+  
+The template file is pre-define to deploy CBS with purity v6.3.5
